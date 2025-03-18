36d28e66a6b5c449cf11bafac57b964628fa55e7
Bill Fehring <bill@billfehring.com>
2025-03-18 00:31:14 -0500

    minor cleanups

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ logging.basicConfig(
 
 # Add console handler for important messages
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
+console_handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(levelname)s - %(message)s')
 console_handler.setFormatter(formatter)
 logging.getLogger().addHandler(console_handler)
